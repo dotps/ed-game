a3e25fa1194e15372c1d58d84f71610c7cd6de1f
Stas Petrov <petrovstas@mail.ru>
2023-03-12 09:01:54 +0500

    Update script AddWordWindow

---
+++ Assets/CodeBase/UI/Windows/Main/AddWordWindow.cs
@@ -0,0 +1,43 @@
+﻿using CodeBase.Services.Progress;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.UI.Windows.Main
+{
+    public class AddWordWindow : BaseWindow
+    {
+        [SerializeField] private Button _addWordButton;
+        [SerializeField] private TMP_InputField _word;
+        [SerializeField] private TMP_InputField _wordTraslation;
+        [SerializeField] private TMP_InputField _wordTranscription;
+        [SerializeField] private TMP_InputField _wordTextSpeech;
+        
+        public void Construct(IProgressService progressService)
+        {
+            base.Construct(progressService);
+        }
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            _addWordButton.onClick.AddListener(SaveWord);
+        }
+
+        private void SaveWord()
+        {
+            Debug.Log(_word.text);
+        }
+
+        protected override void SubscribeUpdates()
+        {
+            // Progress.worldData.collectedLoot.Changed += UpdateTitleText;
+        }
+
+        protected override void UnSubscribeUpdates()
+        {
+            base.UnSubscribeUpdates();
+            // Progress.worldData.collectedLoot.Changed -= UpdateTitleText;
+        }
+    }
+}
